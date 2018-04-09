70b7a4b834c77e7b91b4429d92c85ce061b6b4e7
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 12:51:03 -0700

    Added remove punctuation and all lowercase logic

--- wordcount.py
+++ wordcount.py
@@ -1,14 +1,21 @@
 def word_count(filename):
+    import string
+
+
     text_to_count = open(filename)
 
     word_count_pairs = {}
 
     for line in text_to_count:
-        tokenized_line = line.strip("\n").split(" ")
+        tokenized_line = line.strip("\n").lower().split(" ")
+#.strip(string.punctuation)
+
 
         for word in tokenized_line:
+            #add words to dictionary
+            word = word.strip(string.punctuation)
             word_count_pairs[word] = word_count_pairs.get(word, 0) + 1
-            # word_count_pairs[word] += 1
+                # word_count_pairs[word] += 1
 
     print word_count_pairs
 
